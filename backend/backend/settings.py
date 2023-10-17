68fb936257a5f2e070ffdefa1790f80b0868b85d
Владимир Колпаков <vix-sp@yandex.ru>
2023-10-17 11:09:12 +0300

    Remove slash.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static_backend/'
+STATIC_URL = 'static_backend'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
 # Default primary key field type
