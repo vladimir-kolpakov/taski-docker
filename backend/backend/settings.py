5a4803a2852a748244e15f959ef1626e29ad9300
Владимир Колпаков <vix-sp@yandex.ru>
2023-10-17 12:42:52 +0300

    Add os.dotenv SECRET_KEY.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,10 +1,12 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+os.dotenv('SECRET_KEY', 'secret_key_django_backend')
 
 DEBUG = True
 
